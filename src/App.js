6a320febae2780fc17950f8bf6d31f34938753ff
Migue-San <lx15726@innovaccion.mx>
2023-05-16 17:50:13 -0600

    PORYECYO TERMINADO

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
 import Page404 from './pages/Page404';
 import Header from './components/Header';
+import Post from './pages/Posts';
+import Categoria from './pages/Categoria';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -22,6 +24,16 @@ function App() {
             <Route 
             path='/sobre' 
             element= {<Sobre />}
+          />
+
+          <Route 
+            path='/posts/:id' 
+            element={<Post/>}
+          />
+
+          <Route 
+            path='/categoria/:id/*' 
+            element={<Categoria/>}
           />
 
           <Route 
